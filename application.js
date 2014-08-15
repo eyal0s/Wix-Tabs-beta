4ff918cfc3429a31fb77917395313814fdeba660
eyalbe <eyalbe@gmail.com>
2014-08-15 22:00:00 +0300

    changes to object access

--- application.js
+++ application.js
@@ -1,6 +1,6 @@
 /*By Eyal Benezra*/    
 
-
+//localStorage.clear();
 // analytics
 var _gaq = _gaq || [];
 _gaq.push(['_setAccount', 'UA-53672371-1']);
@@ -14,16 +14,15 @@ _gaq.push(['_trackPageview']);
 
 $(document).ready(function(){
 
-    var feed = "https://dl.dropboxusercontent.com/u/54065586/feed.html";
+    var feed = "https://dl.dropboxusercontent.com/u/54065586/feed[test].json";
     var sitesList = localStorage['sitesList'];
 
     if(typeof sitesList != "undefined" && sitesList.length > 0 && JSON.parse(sitesList).length > 0 ) {
         showSite( JSON.parse(sitesList) );
     } else {
         $.get(feed, function(obj){
-            sitesList = obj.substring(obj.indexOf("<pre style=\"word-wrap: break-word; white-space: pre-wrap;\">") + 62, obj.indexOf("</pre>"));
-           localStorage['sitesList'] = JSON.stringify(sitesList);
-           showSite(JSON.parse(sitesList));
+           localStorage['sitesList'] = JSON.stringify(obj);
+           showSite(JSON.parse(obj));
         });
     }
 
@@ -33,24 +32,19 @@ $(document).ready(function(){
         toggle_tab();
     });
 
-    //  $('.wixtabs').click(function(e){
-    //     e.preventDefault();
-    //     toggle_tab();
-    // });
-
     $('#wixtabs .next').click( function(e){
         e.preventDefault();
          _gaq.push(['_trackEvent', "event", "next"]);
         location.reload(); // Reloads the current document
        
-    })
+    });
 
     $(".refresh").tooltip();
     $('#wixtabs .refresh').click( function(e){
         e.preventDefault();
         localStorage.clear();
         location.reload(); // Reloads the current document
-    })
+    });
 
     $(".contact").tooltip();  
     
@@ -60,28 +54,28 @@ $(document).ready(function(){
 function showSite(sitesList){
 
     // get a new site object from the feed
-
     siteKey = Math.round((sitesList.length - 1) * Math.random());
-    console.log("Debug - picked " + siteKey);
-    console.log("Debug - left " + sitesList.length - 1);
     obj = sitesList[siteKey];
     sitesList.splice(siteKey,1);
     localStorage['sitesList'] = JSON.stringify(sitesList);
     url = obj.url;
+
+    // display the chosen site
     $('iframe.startframe').attr('src', url);
     _gaq.push(['_trackEvent', "event", 'newtab']);
 
 
+/* set toolbar variables */
 
-    // set apps name
-    if(obj.appname){
-        if (obj.appname.toLowerCase().valueOf() == "no app".valueOf()) {
-            appendItem("App", obj.appname.toLowerCase(), "app");
+    // set app name
+    if(obj['app name']){
+        if (obj['app name'].toLowerCase().valueOf() == "no app".valueOf()) {
+            appendItem("App", obj['app name'].toLowerCase(), "app");
         } else {
-            appendItem("App", obj.appname.toLowerCase() + " - " + obj.sitetype.toLowerCase(), "app");
+            appendItem("App", obj['app name'].toLowerCase() + " - " + obj['site type'].toLowerCase(), "app");
         }
         
-    };
+    }
 
     // set package
     // removes double appearance of strings as in "Free Free" and brackets
@@ -95,27 +89,24 @@ function showSite(sitesList){
     }
 
     // set template name
-    var tempname = obj.tempname.toLowerCase();
-    if(tempname && enOnly(tempname)){
-        appendItem("Template", tempname, "temp");
+    if(obj['temp name']){
+        var tempname = obj['temp name'].toLowerCase();
+        if (enOnly(tempname)) {
+            appendItem("Template", tempname, "temp");
+        }
+        
     }
 
-
-        // set date created
-    if (obj.datecreated) {
-        // $li = $('<li></li>').text("published " + dayCount(obj.datecreated) + " days ago");
-        // $li.appendTo('.wixtabs .description');
-        appendItem("Published", dayCount(obj.datecreated) + " days ago");
-     };
+    // set date created
+    if (obj['date created']) {
+        appendItem("Published", dayCount(obj['date created']) + " days ago");
+     }
 
     // set country
     if(obj.country){
         $("<li><b>Made In &nbsp;</b></li>").append("<img class=\"flag\" src=\"layout/flags/" + getCountryName(obj.country) + ".png\""  + "title=\"" + getCountryName(obj.country) + "\"/>").tooltip().appendTo('.wixtabs .description');
     }
 
-
-
-   
     // set sites url href
     if(obj.url){
         $li = $('<li></li>');
@@ -131,14 +122,13 @@ function enOnly(str){
     var pat = /[a-z]/;
     return pat.test(str);
 
-};
+}
 
 // add key, value and id to the description container
 function appendItem(k, v, id){
         $("<li><b>" + k + "</b><br></li>").append(v).attr("id",id).appendTo('.wixtabs .description');
         $("<hr class=\"nomargin\">").appendTo('.wixtabs .description');
-        //
-};
+}
 
 // returns the amount of time passed since date
 function dayCount(date) {
@@ -147,9 +137,9 @@ function dayCount(date) {
     var secondDate = new Date();
 
     return Math.round(Math.abs((firstDate.getTime() - secondDate.getTime())/(oneDay)));
-};
+}
 
-
+// toolbar toggle
 function toggle_tab(){
     $('#wixtabs .toggle').toggleClass('hide');
     $('.wixtabs').toggleClass('hide');
